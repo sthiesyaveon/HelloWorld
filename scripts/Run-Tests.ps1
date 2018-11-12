1802716f1be47ee86c8dbb46c2ee52b1224bbc3a
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-11-12 16:39:52 +0100

    test parameter

--- scripts/Run-Tests.ps1
+++ scripts/Run-Tests.ps1
@@ -6,19 +6,28 @@
     [ValidateSet('bld','dev')]
     [string] $type = "bld",
     [Parameter(Mandatory=$true)]
+    [string] $test,
+    [Parameter(Mandatory=$true)]
     [pscredential] $credential,
     [Parameter(Mandatory=$true)]
     [string] $testResultsFile
 )
 
-# Run Test is a temp solution
-
+# Run Test is a temporary hack
+# We are working on a better solution
 $settings = (Get-Content (Join-Path $PSScriptRoot "..\settings.json") | ConvertFrom-Json)
-$containerName = "$($settings.name)-$type"
-$TempTestResultFile = "C:\ProgramData\NavContainerHelper\Extensions\$containerName\Test Results.xml"
-Invoke-NavContainerCodeunit -containerName $containerName -Codeunitid $settings.TestCodeunitId -CompanyName $settings.TestCompanyName -Argument $TempTestResultFile -MethodName $settings.TestMethodName -TimeZone UTC
-if ($run -eq "AzureDevOps") {
-    (Get-Content -Path $TempTestResultFile -Raw | Convert-CALTestOutputToAzureDevOps).Save($testResultsFile)
-} else {
-    Copy-Item -Path $TempTestResultFile -Destination $testResultsFile -Force
+$runtest = $settings.tests | Where-Object { $_.test -eq $test }
+if ($runtest) {
+    $containerName = "$($settings.name)-$type"
+    $TempTestResultFile = "C:\ProgramData\NavContainerHelper\Extensions\$containerName\Test Results.xml"
+    if ($runtest.type -eq "Codeunit") {
+        Invoke-NavContainerCodeunit -containerName $containerName -Codeunitid $runtest.codeunitId -CompanyName $runtest.companyName -Argument $TempTestResultFile -MethodName $runtest.methodName -TimeZone UTC
+    } else {
+        throw "Unknown test type"
+    }
+    if ($run -eq "AzureDevOps") {
+        (Get-Content -Path $TempTestResultFile -Raw | Convert-CALTestOutputToAzureDevOps).Save($testResultsFile)
+    } else {
+        Copy-Item -Path $TempTestResultFile -Destination $testResultsFile -Force
+    }
 }
