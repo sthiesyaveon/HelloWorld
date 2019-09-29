ecfd3c843d1520f0f09727d1741e564a88f8a796
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2019-09-29 05:50:55 +0200

    CI changes

--- scripts/Read-Settings.ps1
+++ scripts/Read-Settings.ps1
@@ -11,6 +11,17 @@ if ("$version" -eq "")  {
     $version = $settings.versions[0].version
     Write-Host "Version not defined, using $version"
 }
+
+$property = $settings.PSObject.Properties.Match('navContainerHelperVersion')
+if ($property) {
+    $navContainerHelperVersion = $property.Value
+}
+else {
+    $navContainerHelperVersion = $property.Value
+}
+Write-Host "Set navContainerHelperVersion = $navContainerHelperVersion"
+Write-Host "##vso[task.setvariable variable=navContainerHelperVersion]$navContainerHelperVersion"
+
 $appFolders = $settings.appFolders
 Write-Host "Set appFolders = $appFolders"
 Write-Host "##vso[task.setvariable variable=appFolders]$appFolders"
@@ -23,8 +34,17 @@ $imageversion = $settings.versions | Where-Object { $_.version -eq $version }
 if ($imageversion) {
     Write-Host "Set imageName = $($imageVersion.containerImage)"
     Write-Host "##vso[task.setvariable variable=imageName]$($imageVersion.containerImage)"
-    Write-Host "Set alwaysPull = $($imageVersion.alwaysPull)"
-    Write-Host "##vso[task.setvariable variable=alwaysPull]$($imageVersion.alwaysPull)"
+    "alwaysPull","reuseContainer" | ForEach-Object {
+        $property = $imageVersion.PSObject.Properties.Match($_)
+        if ($property) {
+            $propertyValue = $property.Value
+        }
+        else {
+            $propertyValue = $false
+        }
+        Write-Host "Set $_ = $propertyValue"
+        Write-Host "##vso[task.setvariable variable=$_]$propertyValue"
+    }
 }
 else {
     throw "Unknown version: $version"
