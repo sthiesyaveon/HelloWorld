22716b5c24df000135793e44795d873b9425ae73
Freddy Kristiansen <freddyk@microsoft.com>
2020-06-30 14:29:35 +0200

    add fixappjson

---
+++ scripts/FixAppJson.ps1
@@ -0,0 +1,38 @@
+﻿$path = Join-Path $PSScriptRoot ".."
+
+$replaceValues = @{
+    "00000000-0000-0000-0000-000000000001" = [Guid]::NewGuid().ToString()
+    "00000000-0000-0000-0000-000000000002" = [Guid]::NewGuid().ToString()
+    "00000000-0000-0000-0000-000000000003" = [Guid]::NewGuid().ToString()
+    "Default Publisher" = "My Name"
+    "Default App Name" = "My App"
+    "Default Base App Name" = "My Base App"
+    "Default Test App Name" = "My Test App"
+    "1.0.0.0" = "2.0.0.0"
+}
+
+function ReplaceProperty { Param ($object, $property)
+    if ($object.PSObject.Properties.name -eq $property) {
+        $str = $object."$property"
+        if ($replaceValues.ContainsKey($str)) {
+            Write-Host "- change $property from $str to $($replaceValues[$str]) "
+            $object."$property" = $replaceValues[$str]
+        }
+    }
+}
+
+function ReplaceObject { Param($object)
+    "id", "appId", "name", "publisher", "version" | ForEach-Object {
+        ReplaceProperty -object $object -property $_
+    }
+}
+
+Get-ChildItem -Path $path | Where-Object { $_.psIsContainer -and $_.Name -notlike ".*" } | Get-ChildItem -Recurse -filter "app.json" | ForEach-Object {
+    $appJsonFile = $_.FullName
+    $appJson = Get-Content $appJsonFile | ConvertFrom-Json
+    Write-Host -ForegroundColor Yellow $appJsonFile
+    ReplaceObject -object $appJson
+    Write-Host "Check Dependencies"
+    $appJson.dependencies | % { ReplaceObject($_) }
+    $appJson | ConvertTo-Json -Depth 10 | Set-Content $appJsonFile
+}
