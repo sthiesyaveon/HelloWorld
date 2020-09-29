2969c568ab8659928547297d3647a2a4baccbf5d
Freddy Kristiansen <freddyk@microsoft.com>
2020-09-29 11:01:34 +0200

    remove launch json fixes

--- scripts/MySolution.ps1
+++ scripts/MySolution.ps1
@@ -1,22 +1,5 @@
 ﻿$path = Join-Path $PSScriptRoot ".."
 
-$launchJson = [ordered]@{
-    "version" = "0.2.0"
-    "configurations" = @([ordered]@{
-        "type" =  "al"
-        "request" =  "launch"
-        "name" = "Local Sandbox"
-        "server" = "http://HelloWorld-Current"
-        "serverInstance" = "BC"
-        "port" = 7049
-        "tenant" = "default"
-        "authentication" = "UserPassword"
-        "startupObjectId" = 22
-        "startupObjectType" = "Page"
-        "breakOnError" = $true
-    })
-}
-
 $replaceValues = @{
     "00000000-0000-0000-0000-000000000001" = [Guid]::NewGuid().ToString()
     "00000000-0000-0000-0000-000000000002" = [Guid]::NewGuid().ToString()
@@ -53,14 +36,6 @@ Get-ChildItem -Path $path | Where-Object { $_.psIsContainer -and $_.Name -notlik
     Write-Host "Check Dependencies"
     $appJson.dependencies | ForEach-Object { ReplaceObject($_) }
     $appJson | ConvertTo-Json -Depth 10 | Set-Content $appJsonFile
-}
-
-Get-ChildItem -Path $path | Where-Object { $_.psIsContainer -and $_.Name -notlike ".*" } | Get-ChildItem -Recurse -filter "launch.json" | ForEach-Object {
-    $launchJsonFile = $_.FullName
-    $existingLaunchJson = Get-Content $launchJsonFile | ConvertFrom-Json
-    Write-Host -ForegroundColor Yellow $launchJsonFile
-    $launchJson.configurations[0].startupObjectId = $existinglaunchJson.configurations[0].startupObjectId
-    $launchJson | ConvertTo-Json -Depth 10 | Set-Content $launchJsonFile
 }
 
 $settingsFile = Join-Path $PSScriptRoot "settings.json"
