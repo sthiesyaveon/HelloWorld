6835b3d2effc6e3df70b9f005531079bad94ffe6
Freddy Kristiansen <freddyk@microsoft.com>
2020-11-01 14:08:16 +0100

    change bccontainerhelper source

--- scripts/Install-BcContainerHelper.ps1
+++ scripts/Install-BcContainerHelper.ps1
@@ -3,93 +3,49 @@
     [string] $genericImageName = ""
 )
 
-if ($bcContainerHelperVersion -eq "") {
-    $bcContainerHelperVersion = "latest"
+if ($bcContainerHelperVersion -eq "") { $bcContainerHelperVersion = "latest" }
+if ($bccontainerHelperVersion -eq "dev") { $bccontainerHelperVersion = "https://github.com/microsoft/navcontainerhelper/archive/dev.zip" }
+
+if ($bccontainerHelperVersion -like "https://*") {
+    $path = Join-Path $env:TEMP ([Guid]::NewGuid().ToString())
 }
-Write-Host "Use bcContainerHelper Version: '$bcContainerHelperVersion'"
+else {
+    $bcbaseurl = "https://bccontainerhelper.azureedge.net/public"
+    $versionsxml = [xml](New-Object System.Net.WebClient).DownloadString("$($bcbaseurl)?comp=list&restype=container")
 
-$buildMutexName = "bcContainerHelper"
-$buildMutex = New-Object System.Threading.Mutex($false, $buildMutexName)
+    $latestVersion = $versionsxml.EnumerationResults.ChildNodes.Blob.Name | Where-Object { $_ -ne "latest.zip" -and $_ -notlike "*preview*" } | ForEach-Object { $_.replace('.zip','') } | Sort-Object { [Version]$_ } | Select-Object -Last 1
+    $previewVersion = $versionsxml.EnumerationResults.ChildNodes.Blob.Name | Where-Object { $_ -like "*-preview*" } | ForEach-Object { $_.replace('.zip','') } | Sort-Object { [Version]($_.replace('-preview','.')) } | Select-Object -Last 1
+    if ([version]$latestVersion -ge [version]($previewVersion.split('-')[0])) {
+        $previewVersion = $latestVersion
+    }
+    
+    if ($bccontainerHelperVersion -eq "latest") {
+        $bccontainerHelperVersion = $latestVersion
+    }
+    elseif ($bccontainerHelperVersion -eq "preview") {
+        $bccontainerHelperVersion = $previewVersion
+    }
+    $path = Join-Path $env:ProgramFiles "WindowsPowerShell\Modules\BcContainerHelper\$bccontainerHelperVersion"
+    $bccontainerHelperVersion = "$bcbaseurl/$bccontainerHelperVersion.zip"
+}
+
+$bchMutexName = "bcContainerHelper"
+$bchMutex = New-Object System.Threading.Mutex($false, $bchMutexName)
 try {
-    try {
-        if (!$buildMutex.WaitOne(1000)) {
-            Write-Host "Waiting for other process to update BcContainerHelper"
-            $buildMutex.WaitOne() | Out-Null
-            Write-Host "Other process completed"
-        }
-    }
-    catch [System.Threading.AbandonedMutexException] {
-       Write-Host "Other process terminated abnormally"
-    }
-
-    $bcContainerHelperVersion = $bcContainerHelperVersion.Replace('{HOME}',$HOME.TrimEnd('\'))
-    if ($bcContainerHelperVersion -like "?:\*") {
-        if (Test-Path $bcContainerHelperVersion) {
-            $bch = Get-Item (Join-Path $bcContainerHelperVersion '*ContainerHelper.ps1')
-            if ($bch) {
-                Write-Host "Using $bch"
-                . "$bch"
-                return
-            }
-        }
-        $bcContainerHelperVersion = "https://github.com/microsoft/navcontainerhelper/archive/dev.zip"
-    }
-
-    if ($bcContainerHelperVersion -notlike "https://*") {
-        try {
-            Remove-Module -Name BcContainerHelper -Force -ErrorAction SilentlyContinue
-            if ($bcContainerHelperVersion -eq "preview") {
-                Write-Host "Determine latest BcContainerHelper preview version"
-                $latestVersion = (Find-Module -Name bccontainerhelper -AllowPrerelease).Version
-                $bcContainerHelperVersion = $latestVersion.ToString()
-                 Write-Host "BcContainerHelper $bcContainerHelperVersion is the latest preview version"
-            }
-            elseif ($bcContainerHelperVersion -eq "latest") {
-                 Write-Host "Determine latest BcContainerHelper version"
-                 $latestVersion = (Find-Module -Name bccontainerhelper).Version
-                 $bcContainerHelperVersion = $latestVersion.ToString()
-                 Write-Host "BcContainerHelper $bcContainerHelperVersion is the latest version"
-            }
-            $modules = Get-InstalledModule -Name bccontainerhelper -ErrorAction SilentlyContinue -AllVersions -AllowPrerelease
-            if ($modules | Where-Object { $_.Version -eq $bcContainerHelperVersion }) {
-                if ($bcContainerHelperVersion -like "*preview*") {
-                    Import-Module -Name BcContainerHelper -DisableNameChecking
-                }
-                else {
-                    Import-Module -Name BcContainerHelper -RequiredVersion $bcContainerHelperVersion -DisableNameChecking
-                }
-            }
-            else {
-                if ($bcContainerHelperVersion -like "*preview*") {
-                    Install-Module -Name BcContainerHelper -AllowPrerelease -Force
-                    Import-Module -Name BcContainerHelper -Force -DisableNameChecking
-                }
-                else {
-                    Install-Module -Name BcContainerHelper -RequiredVersion $bcContainerHelperVersion -Force
-                    Import-Module -Name BcContainerHelper -RequiredVersion $bcContainerHelperVersion -Force -DisableNameChecking
-                }
-            }
-        }
-        catch {
-            $bcContainerHelperVersion = "https://github.com/microsoft/navcontainerhelper/archive/master.zip"
-        }
-    }
-    if ($bcContainerHelperVersion -like "https://*") {
-        Remove-Module BcContainerHelper -ErrorAction SilentlyContinue
-        $tempName = Join-Path $env:TEMP $containerName
-        Remove-Item $tempName -Recurse -Force -ErrorAction SilentlyContinue
-        Remove-Item "$tempName.zip" -Force -ErrorAction SilentlyContinue
-        Write-Host "Downloading $bcContainerHelperVersion"
-        (New-Object System.Net.WebClient).DownloadFile($bcContainerHelperVersion, "$tempName.zip")
+    try { $bchMutex.WaitOne() | Out-Null } catch {}
+    if (!(Test-Path $path)) {
+        $tempName = Join-Path $env:TEMP ([Guid]::NewGuid().ToString())
+        Write-Host "Downloading $bccontainerHelperVersion"
+        (New-Object System.Net.WebClient).DownloadFile($bccontainerHelperVersion, "$tempName.zip")
         Expand-Archive -Path "$tempName.zip" -DestinationPath $tempName
-        $modulePath = (Get-Item (Join-Path $tempName "*\BcContainerHelper.psm1")).FullName
-        Write-Host $modulePath
-        Import-Module $modulePath -DisableNameChecking
+        $folder = (Get-Item -Path (Join-Path $tempName '*')).FullName
+        [System.IO.Directory]::Move($folder,$path)
     }
 }
 finally {
-    $buildMutex.ReleaseMutex()
+    $bchMutex.ReleaseMutex()
 }
+. (Join-Path $path "BcContainerHelper.ps1")
 
 if ($genericImageName) {
     $bcContainerHelperConfig.genericImageName = $genericImageName
