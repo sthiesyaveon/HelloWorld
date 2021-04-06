10a3d96620cc9411d16db59d88223429243c8ef5
Freddy Kristiansen <freddyk@microsoft.com>
2021-04-06 06:59:16 +0200

    add cloud

--- scripts/Cloud-DevEnv.ps1
+++ scripts/Cloud-DevEnv.ps1
@@ -1,10 +1,9 @@
-﻿Param(
+Param(
     [Parameter(Mandatory=$false)]
-    [string] $version = "ci",
-    [Parameter(Mandatory=$false)]
-    [int] $appBuild = ([int32]::MaxValue),
-    [Parameter(Mandatory=$false)]
-    [int] $appRevision = 0
+    [string] $version = "cloud",
+    [Parameter(Mandatory=$true)]
+    [string] $environmentName,
+    [switch] $reuseEnvironment
 )
 
 $baseFolder = (Get-Item (Join-Path $PSScriptRoot "..")).FullName
@@ -21,6 +20,47 @@ Get-AzKeyVaultSecret -VaultName $vaultNameForLocal | ForEach-Object {
 $licenseFile = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($licenseFileSecret.SecretValue))
 $insiderSasToken = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($insiderSasTokenSecret.SecretValue))
 $credential = New-Object pscredential 'admin', $passwordSecret.SecretValue
+$refreshToken = $BcSaasRefreshTokenSecret.SecretValueText
+$authContext = $null
+
+if ($refreshToken -and $environmentName) {
+    $authContext = New-BcAuthContext -refreshToken $refreshToken
+    $existingEnvironment = Get-BcEnvironments -bcAuthContext $authContext | Where-Object { $_.Name -eq $environmentName }
+    if ($existingEnvironment) {
+        if ($existingEnvironment.type -ne "Sandbox") {
+            throw "Environment $environmentName already exists and it is not a sandbox environment"
+        }
+        if (!$reuseEnvironment) {
+            Remove-BcEnvironment -bcAuthContext $authContext -environment $environmentName
+            $existingEnvironment = $null
+        }
+    }
+    if ($existingEnvironment) {
+        $countryCode = $existingEnvironment.CountryCode.ToLowerInvariant()
+        $baseApp = Get-BcPublishedApps -bcAuthContext $authContext -environment $environmentName | Where-Object { $_.Name -eq "Base Application" }
+    }
+    else {
+        $countryCode = $artifact.Split('/')[3]
+        New-BcEnvironment -bcAuthContext $authContext -environment $environmentName -countryCode $countrycode -environmentType "Sandbox" | Out-Null
+        do {
+            Start-Sleep -Seconds 10
+            $baseApp = Get-BcPublishedApps -bcAuthContext $authContext -environment $environmentName | Where-Object { $_.Name -eq "Base Application" }
+        } while (!($baseApp))
+        $baseapp | Out-Host
+    }
+
+    $artifact = Get-BCArtifactUrl `
+        -country $countryCode `
+        -version $baseApp.Version `
+        -select Closest
+    
+    if ($artifact) {
+        Write-Host "Using Artifacts: $artifact"
+    }
+    else {
+        throw "No artifacts available"
+    }
+}
 
 $allTestResults = "testresults*.xml"
 $testResultsFile = Join-Path $baseFolder "TestResults.xml"
@@ -33,28 +73,23 @@ Run-AlPipeline `
     -pipelineName $pipelineName `
     -containerName $containerName `
     -imageName $imageName `
+    -bcAuthContext $authContext `
+    -environment $environmentName `
     -artifact $artifact.replace('{INSIDERSASTOKEN}',$insiderSasToken) `
     -memoryLimit $memoryLimit `
     -baseFolder $baseFolder `
     -licenseFile $licenseFile `
     -installApps $installApps `
     -installTestApps $installTestApps `
-    -previousApps $previousApps `
     -appFolders $appFolders `
     -testFolders $testFolders `
-    -doNotRunTests:$doNotRunTests `
     -testResultsFile $testResultsFile `
     -testResultsFormat 'JUnit' `
     -installTestRunner:$installTestRunner `
     -installTestFramework:$installTestFramework `
     -installTestLibraries:$installTestLibraries `
     -installPerformanceToolkit:$installPerformanceToolkit `
-    -enableCodeCop:$enableCodeCop `
-    -enableAppSourceCop:$enableAppSourceCop `
-    -enablePerTenantExtensionCop:$enablePerTenantExtensionCop `
-    -enableUICop:$enableUICop `
-    -AppSourceCopMandatoryAffixes $appSourceCopMandatoryAffixes `
-    -AppSourceCopSupportedCountries $appSourceCopSupportedCountries `
-    -additionalCountries $additionalCountries `
     -credential $credential `
-    -appBuild $appBuild -appRevision $appRevision
+    -doNotRunTests `
+    -useDevEndpoint `
+    -updateLaunchJson "Cloud Sandbox" `
